7e4f914f764d021ce3ec13ca755278f673ccec55
abdul <Ahmedabdul362@gmail.com>
2025-02-06 10:50:13 -0500

    todo 3

--- src/main/java/com/mycompany/studentlst/StudentLst.java
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -8,27 +8,27 @@ import java.util.Scanner;
 public class StudentLst {
 
     public static void main(String[] args) {
-        Student[] students= new Student[2];
+        Student[] students = new Student[2];
         Scanner inScanner = new Scanner(System.in);
-        
-        for(int i=0; i< students.length;i++){
+
+        for (int i = 0; i < students.length; i++) {
             System.out.print("Enter student's name: ");
             String name = inScanner.nextLine();
             Student student = new Student(name);
-            
-            students[i]= student;
-            
+
+            students[i] = student;
         }
         System.out.println(printOutput(students));
     }
-    
-    public static String printOutput(Student[] students){
-        String str="";
-        
-        //To Do 03: complete the implementation of this method  
-        
+
+    public static String printOutput(Student[] students) {
+        String str = "";
+
+        // To Do 03: Complete the implementation of this method
+        for (Student student : students) {
+            str += student.getName() + "\n";  // Add each student's name followed by a newline
+        }
+
         return str;
     }
-    
-    
 }
